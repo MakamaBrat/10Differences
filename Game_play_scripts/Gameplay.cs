246dae65e68c32cb41341ad8d9606d66eef8fe93
MakamaBrat <93373258+MakamaBrat@users.noreply.github.com>
2021-12-22 14:29:33 +0200

    add scripts

---
+++ Game_play_scripts/Gameplay.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Tests;
+
+public class Gameplay : MonoBehaviour
+{
+    public Ad1 ad1;
+    public Ad2 ad2;
+    public Animator loop;
+    public Sprite[] loop_sprites;
+    public Sprite[] sprites_up;
+    public Sprite[] sprites_down;
+    public Transform[] l;
+    public int current = 0;
+    public GameObject upimage;
+    public GameObject downimage;
+    public GameObject[] marks;
+    public GameObject[] marks2;
+    public bool miss;
+    int reklamacount = 0;
+    public Levelmanager levelmanager;
+    public int amount = 0;
+    public int health;
+    public int dif;
+    public GameObject[] massages;
+    public GameObject[] Lose_and_win;
+    public GameObject rehealth_button;
+    public Miss[] misses;
+    public Sound sound;
+   
+
+    public Transform[] mark_dif;
+ 
+
+
+    void Start()
+    {
+
+      
+        l = gameObject.GetComponent<Transform>().GetComponentsInChildren<Transform>();
+
+        int b = 0;
+
+        Allmarkactive all = new Allmarkactive();
+        all.Marks_active(marks);
+
+
+    }
+    private void Awake()
+    {
+        List<GameObject> m1 = new List<GameObject>();
+        List<GameObject> m2 = new List<GameObject>();
+        Transform[] game = gameObject.GetComponentsInChildren<Transform>();
+        foreach (Transform l in game)
+        {
+            if (l.gameObject.tag == "levelup")
+            { m1.Add(l.gameObject); }
+            if (l.gameObject.tag == "leveldown")
+            { m2.Add(l.gameObject); }
+            marks = m1.ToArray();
+            marks2 = m2.ToArray();
+
+        }
+        misses[0].make_start();
+        misses[1].make_start();
+        syn_content();
+        
+    }
+    public void syn_content()
+    {
+        
+        Sprite[] spr1 = new Sprite[100];
+        Sprite[] spr2 = new Sprite[100];
+        sprites_up = Resources.LoadAll<Sprite>("levels_up");
+        sprites_down = Resources.LoadAll<Sprite>("levels_down");
+
+        int k = 0;
+        foreach (Sprite sp in sprites_up)
+        {
+       
+            spr1[int.Parse(sp.name.Substring(0, sp.name.Length - 2)) - 1] = sprites_up[k];
+
+
+            k++;
+
+        }
+        k = 0;
+        foreach (Sprite sp in sprites_down)
+        {
+          
+            spr2[int.Parse(sp.name.Substring(0, sp.name.Length - 2)) - 1] = sprites_down[k];
+
+
+            k++;
+
+        }
+
+
+        sprites_up = spr1;
+        sprites_down = spr2;
+
+    }
+    private void OnEnable()
+    {
+        imageupdate();
+        if (levelmanager.mag > 0 || levelmanager.coin>=10)
+         StartCoroutine(no_mark()); 
+        if (levelmanager.first_loop_click == 0)
+        { make_loop_anim(false);
+            levelmanager.first_loop_click = 0;
+            levelmanager.save_game();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0) && miss == true && !massages_ative())
+        {missed();
+            
+        }
+
+
+
+
+    }
+   
+    public void imageupdate()
+    {
+        upimage.GetComponent<Image>().sprite = sprites_up[current];
+        downimage.GetComponent<Image>().sprite = sprites_down[current];
+        for (int i = 0; i < marks.Length; i++)
+        {
+            if (i == current)
+            {
+                marks[i].SetActive(true);
+                marks2[i].SetActive(true);
+            }
+            else
+            {
+                marks[i].SetActive(false);
+                marks2[i].SetActive(false);
+            }
+        }
+    }
+
+  
+
+    public void regame()
+    {
+
+        if (levelmanager.super == 0)
+        { health = 10; }
+        else { health = 999; }
+        dif = 0;
+  
+
+        Transform[] mark_dif = marks[current].GetComponentsInChildren<Transform>();
+
+
+        int b = 0;
+        foreach (Transform m in mark_dif)
+        {
+            if (b > 0)
+            {
+                m.gameObject.GetComponent<SpriteRenderer>().color = m.gameObject.GetComponent<Marks>().color1;
+                m.gameObject.GetComponent<Marks>().cheked = false;
+            }
+            b++;
+        }
+
+        Transform[] mark_dif2 = marks2[current].GetComponentsInChildren<Transform>();
+
+
+       b = 0;
+        foreach (Transform m in mark_dif2)
+        {
+            if (b > 0)
+            {
+                m.gameObject.GetComponent<SpriteRenderer>().color = m.gameObject.GetComponent<Marks>().color1;
+                m.gameObject.GetComponent<Marks>().cheked = false;
+            }
+            b++;
+        }
+    }
+
+    public void rehealth()
+    {
+        ad2.rek_id = 4;
+        sound.stop_sound();
+        ad2.UserChoseToWatchAd();
+        rehealth_button.SetActive(false);
+
+    }
+
+    public void eqmark()
+    {
+        Marks[] mark_dif1 = marks[current].GetComponentsInChildren<Marks>();
+        Marks[] mark_dif2 = marks2[current].GetComponentsInChildren<Marks>();
+        levelmanager.mainsound.makesound(0);
+        for (int i = 0; i < mark_dif1.Length; i++)
+        {
+            bool a = mark_dif1[i].opened();
+            bool b = mark_dif2[i].opened();
+            if (a == false && a != b)
+            { mark_dif2[i].makesprite(); }
+            if (b == false && a != b)
+            { mark_dif1[i].makesprite(); }
+        }
+
+        StopCoroutine(no_mark());
+        StartCoroutine(no_mark());
+
+    }
+
+    public bool massages_ative()
+    {
+        bool k = false;
+        foreach (GameObject m in massages)
+        {
+            if (m.active)
+            {
+                k = true;
+            }
+        
+
+        }
+        return k;
+    }
+   
+
+
+    public void click()
+    {
+
+
+        dif += 1;
+
+
+
+        
+
+
+
+
+    }
+
+    public void maguse()
+    {
+        StopCoroutine(no_mark());
+        StartCoroutine(no_mark());
+
+       var k = loop.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        if(k=="mag_act")
+        {
+            make_loop_anim(true);
+            if (levelmanager.first_loop_click==0)
+            { levelmanager.first_loop_click = 1;
+                levelmanager.save_game();
+            }
+            
+        }
+
+
+        if (dif <=10)
+        {
+            
+                if (levelmanager.mag > 0)
+                { levelmanager.mag -= 1; }
+                else if (levelmanager.coin >=10 )
+                 { levelmanager.coin -= 10; }
+                else if(levelmanager.coin<10 && levelmanager.mag<=0)
+                { return; }
+             
+                Marks[] mark_dif = marks[current].GetComponentsInChildren<Marks>();
+                foreach (Marks m in mark_dif)
+                {
+                    if (m.cheked == false)
+                    {
+                        m.founded();
+                        eqmark();
+                        levelmanager.mainsound.makesound(0);
+                        if (dif == 10)
+                        {
+
+
+                            opennextlevel();
+
+
+
+                        }
+
+                        return;
+                    }
+
+
+
+
+
+
+
+                
+
+                }
+        }
+
+
+
+    }
+
+    public void opennextlevel()
+    {
+       
+     
+    
+        if (current <levelmanager.levelopen.Length)
+        {
+           
+                levelmanager.levelopen[current+1] = 1;
+           
+
+           
+            
+        }
+        
+        levelmanager.save_game();
+        StartCoroutine(after_last_mark());
+
+    }
+
+    public void missed()
+    {
+      
+  
+        health -= 1;
+        if(health==5)
+        { levelmanager.mewarn(0);
+            levelmanager.mainsound.makesound(1);
+        }
+        if (health == 2)
+        { levelmanager.mewarn(1);
+            levelmanager.mainsound.makesound(1);
+        }
+        if (health == 1)
+        { levelmanager.mewarn(2);
+            levelmanager.mainsound.makesound(1);
+        }
+        if (health == 0)
+        {
+            if (reklamacount == 0)
+            { 
+                reklamacount = 2;
+            }
+            else
+            {
+                reklamacount -= 1;
+            }
+            Lose_and_win[0].SetActive(true);
+            sound.makesound(3);
+            if (ad2.rewardedAd.IsLoaded())
+            {
+                rehealth_button.SetActive(true);
+               
+            }
+            else { rehealth_button.SetActive(false); }
+
+
+        }
+    }
+
+    public IEnumerator after_last_mark()
+    {
+        yield return new WaitForSeconds(0.7f);
+        sound.makesound(4);
+        Lose_and_win[1].SetActive(true);
+    }
+    
+    public void after_win_exit()
+    {
+        Lose_and_win[1].SetActive(false);
+        levelmanager.levelgone[current] = 1;
+        levelmanager.coin += 4;
+        gameObject.SetActive(false);
+        levelmanager.golevelchoise();
+        levelmanager.levelchoise_control.GetComponent<Menu_choise>().stateupdate();
+        sound.stop_sound();
+
+    }
+
+    public void after_lose_exit()
+    {
+       
+        Lose_and_win[0].SetActive(false);
+        make_loop_anim(true);
+            levelmanager.mesok1menu();
+        sound.stop_sound();
+        ad1.GameOver();
+        reklamacount = 2;
+    }
+
+
+    public void make_loop_anim(bool state)
+    {
+        if(state)
+        {
+            loop.Play("mag_state");
+            loop.GetComponent<Image>().sprite = loop_sprites[0];
+        }
+        else {
+            loop.Play("mag_act");
+            loop.GetComponent<Image>().sprite = loop_sprites[1];
+        }
+
+    }
+
+
+    IEnumerator no_mark()
+    {
+        yield return new WaitForSeconds(10f);
+        make_loop_anim(false);
+        if(dif<10)
+        { StartCoroutine(no_mark()); }
+    }
+}
+
+
---
+++ Game_play_scripts/Levelmanager.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Levelmanager : MonoBehaviour
+{
+  
+
+    [Header("Layouts")]
+    public GameObject mainmenu;
+    public GameObject levelchoise;
+    public GameObject levelchoise_control;
+    public GameObject gameplay;
+    public GameObject settings;
+    public GameObject shop;
+    public GameObject aboutus;
+    public Ad3 banner;
+    int b = 0;
+    [Header("Data")]
+    public int coin;
+    public int mag;
+    public int super;
+    public int calendar_day;
+    public int first_loop_click;
+    public int everyday_step;
+    public int[] levelopen;
+    public int[] levelgone;
+
+    [Header("Messeges")]
+    public GameObject exit;
+    public GameObject ok1;
+    public GameObject ok2;
+    public GameObject warn;
+    public GraphicRaycaster raycaster;
+    public SpriteRenderer spwarn;
+    public Sprite[] sprite;
+
+
+    public GameObject panel;
+    public Sound mainsound;
+
+
+    public Text texts_coin;
+    public Text texts_mag;
+   
+    
+
+    [Header("Settings")]
+    public float sound;
+    public float music;
+
+    void Start()
+    {
+
+        mainsound = gameObject.GetComponent<Sound>();
+        levelgone = new int[levelopen.Length];
+        
+        loadsaves();
+
+        
+        levelchoise_control.GetComponent<Menu_choise>().Start();
+    }
+
+    public void loadsaves()
+    {
+        b = 0;
+        foreach (int k in levelopen)
+        {
+            if (PlayerPrefs.HasKey("levelopen" + b))
+                levelopen[b] = PlayerPrefs.GetInt("levelopen" + b);
+            else
+            {
+                levelopen[b] = 0;
+            }
+          ;
+            b++;
+        }
+        b = 0;
+        foreach (int k in levelgone)
+        {
+            if (PlayerPrefs.HasKey("levelgone" + b))
+                levelgone[b] = PlayerPrefs.GetInt("levelgone" + b);
+            else
+            {
+                levelgone[b] = 0;
+            }
+            b++;
+        }
+        levelopen[0] = 1;
+        if (PlayerPrefs.HasKey("coin"))
+        { coin = PlayerPrefs.GetInt("coin"); }
+        else
+        {
+            coin = 30;
+        }
+
+        if (PlayerPrefs.HasKey("mag"))
+        { mag = PlayerPrefs.GetInt("mag"); }
+        else
+        {
+            mag = 10;
+        }
+
+        if (PlayerPrefs.HasKey("super"))
+            super = PlayerPrefs.GetInt("super");
+        else
+        {
+            super = 0;
+        }
+        if (PlayerPrefs.HasKey("calendar_day"))
+            calendar_day = PlayerPrefs.GetInt("calendar_day");
+        else
+        {
+           calendar_day = 0;
+        }
+
+        if (PlayerPrefs.HasKey("first_loop_click"))
+            everyday_step = PlayerPrefs.GetInt("first_loop_click");
+        else
+        {
+            everyday_step = 0;
+        }
+
+        if (PlayerPrefs.HasKey("first_loop_click"))
+            everyday_step = PlayerPrefs.GetInt("first_loop_click");
+        else
+        {
+            everyday_step = 0;
+        }
+    }
+       
+    void Update()
+    {
+        if (texts_coin.text != coin.ToString())
+        { texts_coin.text = coin.ToString(); }
+
+        if (texts_mag.text != mag.ToString())
+        { texts_mag.text = mag.ToString(); }
+
+
+    }
+    public void golevelchoise()
+    {
+       
+
+        mainmenu.SetActive(false);
+        levelchoise.SetActive(true);
+        gameplay.SetActive(false);
+            settings.SetActive(false);
+        shop.SetActive(false);
+        levelchoise_control.GetComponent<Menu_choise>().stateupdate();
+
+    }
+
+    public void gogameplay()
+    {
+
+        mainmenu.SetActive(false);
+        levelchoise.SetActive(false);
+        gameplay.SetActive(true);
+        settings.SetActive(false);
+        shop.SetActive(false);
+        aboutus.SetActive(false);
+
+        
+    }
+
+    public void gosettings()
+    {
+        if (!gameplay.GetComponent<Gameplay>().Lose_and_win[0].activeInHierarchy && !gameplay.GetComponent<Gameplay>().Lose_and_win[1].activeInHierarchy)
+        {
+
+            settings.SetActive(true);
+            raycaster.enabled = false;
+        }
+
+        
+    }
+
+    public void exitsettings()
+    {
+
+
+        settings.SetActive(false);
+        aboutus.SetActive(false);
+        raycaster.enabled = true;
+
+
+
+    }
+
+    public void goshop()
+    {
+       
+        mainmenu.SetActive(false);
+        levelchoise.SetActive(false);
+        gameplay.SetActive(false);
+        settings.SetActive(false);
+        shop.SetActive(true);
+        aboutus.SetActive(false);
+
+    }
+
+    public void goaboutus()
+    {
+        if (!gameplay.GetComponent<Gameplay>().Lose_and_win[0].activeInHierarchy && !gameplay.GetComponent<Gameplay>().Lose_and_win[1].activeInHierarchy)
+        {
+            aboutus.SetActive(true);
+            raycaster.enabled = false;
+        }
+    }
+
+
+    public void gomainmenu()
+    {
+        if (!gameplay.GetComponent<Gameplay>().Lose_and_win[0].activeInHierarchy && !gameplay.GetComponent<Gameplay>().Lose_and_win[1].activeInHierarchy)
+        {
+            if (!GameplayActive())
+            {
+                levelchoise.SetActive(false);
+                gameplay.SetActive(false);
+                settings.SetActive(false);
+                shop.SetActive(false);
+                aboutus.SetActive(false);
+                mainmenu.SetActive(true);
+                raycaster.enabled = true;
+            }
+            else { mesexit(); }
+        }
+    }
+    public bool GameplayActive()
+    { return gameplay.gameObject.active; }
+
+    public void maguse()
+    {
+        
+        if (GameplayActive())
+        {
+           
+            gameplay.GetComponent<Gameplay>().maguse();
+        }
+
+
+    }
+
+    public void magupdate()
+    {
+        texts_mag.text =mag.ToString();
+
+    }
+
+
+    public void gameexit()
+    { Application.Quit(); }
+
+    public void mesexit()
+    {
+        if (gameplay.GetComponent<Gameplay>().dif < 10)
+        {
+            raycaster.enabled = false;
+            exit.SetActive(true);
+        }
+
+
+    }
+
+    public void meok1()
+    {
+        exit.SetActive(false);
+
+        raycaster.enabled = false;
+        ok1.SetActive(true);
+
+
+    }
+
+    public void mesok1menu()
+    {
+        banner.RequestBanner();
+        ok1.SetActive(false);
+        levelchoise.SetActive(true);
+        gameplay.SetActive(false);
+        settings.SetActive(false);
+        shop.SetActive(false);
+        aboutus.SetActive(false);
+        mainmenu.SetActive(false);
+        save_game();
+        mainsound.makesound(2);
+        raycaster.enabled = true;
+    }
+
+    public void meok2()
+    {
+        exit.SetActive(false);
+        raycaster.enabled = false;
+        ok2.SetActive(true);
+
+
+    }
+
+    public void mewarn(int i)
+    {
+        raycaster.enabled = false;
+        warn.SetActive(true);
+        if (i == 0)
+           spwarn.sprite=sprite[0] ;
+        if (i == 1)
+            spwarn.sprite = sprite[1];
+        if (i == 2)
+            spwarn.sprite = sprite[2];
+
+    }
+
+    public void reumegame()
+    {
+        StartCoroutine(i_reumegame());
+           
+        
+
+
+    }
+
+    IEnumerator i_reumegame()
+    {
+        yield return new WaitForSeconds(0.1f);
+        raycaster.enabled = true;
+        ok2.SetActive(false);
+        ok1.SetActive(false);
+        warn.SetActive(false);
+
+    }
+
+ 
+
+
+
+   public void save_game()
+    {
+        b = 0;
+        foreach (int k in levelopen)
+        {
+            
+                PlayerPrefs.SetInt("levelopen" + b,levelopen[b]);
+           
+            
+            b++;
+        }
+        b = 0;
+        foreach (int k in levelgone)
+        {
+           
+               PlayerPrefs.SetInt("levelgone" + b,levelgone[b]);
+            
+            b++;
+        }
+        PlayerPrefs.SetInt("coin", coin);
+        PlayerPrefs.SetInt("mag", mag);
+        PlayerPrefs.SetInt("super", super);
+        PlayerPrefs.SetInt("calendar_day", calendar_day);
+        PlayerPrefs.SetInt("everyday_step", everyday_step);
+        PlayerPrefs.SetInt("first_loop_click", first_loop_click);
+    }
+
+
+}
